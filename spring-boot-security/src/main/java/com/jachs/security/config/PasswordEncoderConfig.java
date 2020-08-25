ecbf14b222846b641784e4ba64f80d2c714b3f3c
jachs <799516423@qq.com>
2020-08-25 16:53:11 +0800

    AAAAAA

--- spring-boot-security/src/main/java/com/jachs/security/config/PasswordEncoderConfig.java
+++ spring-boot-security/src/main/java/com/jachs/security/config/PasswordEncoderConfig.java
@@ -1,29 +1,51 @@
 package com.jachs.security.config;
 
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 /****
  * 密码加密
+ * 
  * @author zhanchaohan
- *
+ * @see https://docs.spring.io/spring-security/site/docs/current/reference/html5/#authentication-password-storage
  */
 @Component
 public class PasswordEncoderConfig implements PasswordEncoder {
-	//默认提供加密对象
-	public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    //默认提供加密对象
+    public BCryptPasswordEncoder passwordEncoder () {
+        return new BCryptPasswordEncoder ();
     }
-	@Override
-	public String encode(CharSequence rawPassword) {
-		// 加密方法可以根据自己的需要修改
-		return rawPassword.toString();
-	}
 
-	@Override
-	public boolean matches(CharSequence rawPassword, String encodedPassword) {
-		return encode(rawPassword).equals(encodedPassword);
-	}
+    @Override
+    public String encode ( CharSequence rawPassword ) {
+        // 加密方法可以根据自己的需要修改
+        
+     // Create an encoder with strength 16
+//        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(16);
+//        String result = encoder.encode(rawPassword);
+        
+     // Create an encoder with all the defaults
+//        Argon2PasswordEncoder encoder = new Argon2PasswordEncoder();
+//        String result = encoder.encode("myPassword");
+        
+     // Create an encoder with all the defaults
+//        Pbkdf2PasswordEncoder encoder = new Pbkdf2PasswordEncoder();
+//        String result = encoder.encode("myPassword");
+        
+     // Create an encoder with all the defaults
+//        SCryptPasswordEncoder encoder = new SCryptPasswordEncoder();
+//        String result = encoder.encode("myPassword");
+        
+        return rawPassword.toString ();
+    }
+
+    @Override
+    public boolean matches ( CharSequence rawPassword , String encodedPassword ) {
+        return encode ( rawPassword ).equals ( encodedPassword );
+    }
 
 }

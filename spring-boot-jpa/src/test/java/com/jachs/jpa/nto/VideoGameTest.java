208ed8549b200aed462904f25abb890197eeca31
jachs <799516423@qq.com>
2020-12-10 17:30:56 +0800

    bda

--- spring-boot-jpa/src/test/java/com/jachs/jpa/nto/VideoGameTest.java
+++ spring-boot-jpa/src/test/java/com/jachs/jpa/nto/VideoGameTest.java
@@ -4,8 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.jachs.jpa.dao.nto.PlayerRepository;
 import com.jachs.jpa.dao.nto.VideoGameRepository;
+import com.jachs.jpa.entity.nto.Player;
 import com.jachs.jpa.entity.nto.VideoGame;
+
 /**
  * @author zhanchaohan
  * 
@@ -13,14 +16,46 @@ import com.jachs.jpa.entity.nto.VideoGame;
 @SpringBootTest
 public class VideoGameTest {
     @Autowired
-    private VideoGameRepository VideoGameRepository;
+    private VideoGameRepository videoGameRepository;
+    @Autowired
+    private PlayerRepository playerRepository;
     
+    //单条添加
     @Test
-    public void testAddOne() {
-        VideoGame vg=new VideoGame();
+    public void testAddOne () {
+        Player p1 = new Player ();
+        p1.setLevel ( 2 );
+        p1.setPlayerUserName ( "新手玩家" );
+
+        playerRepository.save ( p1 );
+        
+        VideoGame vg = new VideoGame ();
         vg.setGameName ( "" );
         vg.setGamePrice ( 5412 );
+
+       
+        vg.setPlayer ( p1 );
+        videoGameRepository.save ( vg );
+    }
+    @Test
+    public void testAddMany() {
+        Player p1 = new Player ();
+        p1.setLevel ( 3 );
+        p1.setPlayerUserName ( "Level3玩家" );
+
+        playerRepository.save ( p1 );
         
-//        vg.setPlayer ( Player );
+        VideoGame vg = new VideoGame ();
+        vg.setGameName ( "LOL" );
+        vg.setGamePrice ( 5412 );
+
+        VideoGame vg1 = new VideoGame ();
+        vg1.setGameName ( "OverWatch" );
+        vg1.setGamePrice ( 2754 );
+        
+        vg.setPlayer ( p1 );
+        vg1.setPlayer ( p1 );
+        videoGameRepository.save ( vg );
+        videoGameRepository.save ( vg1 );
     }
 }

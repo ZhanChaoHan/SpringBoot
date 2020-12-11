d0001063c4db5de7ba43790ce1b74380fcef5810
jachs <799516423@qq.com>
2020-12-11 10:07:44 +0800

    CCSSA

---
+++ spring-boot-jpa/src/main/java/com/jachs/jpa/dao/ntm/ExamRepository.java
@@ -0,0 +1,13 @@
+package com.jachs.jpa.dao.ntm;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jachs.jpa.entity.ntm.Exam;
+
+/**
+ * @author zhanchaohan
+ * 
+ */
+public interface ExamRepository extends JpaRepository<Exam,Integer>{
+
+}
---
+++ spring-boot-jpa/src/main/java/com/jachs/jpa/dao/ntm/ExamineeRepository.java
@@ -0,0 +1,13 @@
+package com.jachs.jpa.dao.ntm;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jachs.jpa.entity.ntm.Examinee;
+
+/**
+ * @author zhanchaohan
+ * 
+ */
+public interface ExamineeRepository extends JpaRepository<Examinee,Integer>{
+
+}

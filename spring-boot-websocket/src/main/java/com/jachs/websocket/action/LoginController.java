6d1898a4edc94b9b34e017a114cd2dc43d7e648f
jachs <799516423@qq.com>
2021-01-06 17:56:37 +0800

    'ertze'

--- spring-boot-websocket/src/main/java/com/jachs/websocket/action/LoginController.java
+++ spring-boot-websocket/src/main/java/com/jachs/websocket/action/LoginController.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.google.gson.Gson;
 import com.jachs.websocket.entity.Message;
@@ -28,23 +29,24 @@ import com.jachs.websocket.entity.Status;
 public class LoginController {
 	private static final Log logger = LogFactory.getLog(LoginController.class);
 	
-	@RequestMapping(value = "/index.do", method = RequestMethod.GET,produces="text/plain;charset=UTF-8")
-	public String index() {
+	@RequestMapping(value = "/index.do")
+	public ModelAndView index() {
 		try {
 			logger.info("初始化登陆页面...");
 //			File path =new File(ResourceUtils.getURL("classpath:").getPath());
-			
-			
+//			
+//			
 //			BufferedWriter fileWriter=new BufferedWriter(new FileWriter(URLDecoder.decode(path.getAbsolutePath(), "UTF-8")+File.separator+"static"+File.separator+"js"+File.separator+"Status.json"));
-//			Gson gson=new Gson();
-//			String []json=new String []{gson.toJson(Status.values()),gson.toJson(new Message("",false,Status.SENDMESS,""))};
+			Gson gson=new Gson();
+			String []json=new String []{gson.toJson(Status.values()),gson.toJson(new Message("",false,Status.SENDMESS,""))};
 //			fileWriter.write(gson.toJson(json));
 //			fileWriter.close();
-			return "/html/chess/chess";
+			
+			return new ModelAndView ( "chess","userInfo",gson.toJson(json) );
 		} catch (Exception e) {
 			e.printStackTrace();
 			logger.info("初始化登陆页失败...");
-			return "/login/error";
+			return null;
 		}
 	}
 	
